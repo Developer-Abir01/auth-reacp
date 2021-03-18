5a2b1717fb1836218a3d3643395fe8c8e1c849bc
Abirk2day <abirk2day@gmail.com>
2021-03-18 10:08:37 +0600

    authcation is complet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import firebaseConfig from './fierbase.config';
+import { useState } from 'react';
+
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+}
 
 function App() {
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var ghProvider = new firebase.auth.GithubAuthProvider();
+  
+  const [user, setUser] = useState({})
+
+
+  const handleGoogleSingIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        setUser(user)
+
+        // ...
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage ,errorCode , email , credential)
+      });
+  }
+
+
+
+
+  const handleFbSingIn = () => {
+
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        setUser(user)
+       
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorMessage ,errorCode , email , credential)
+
+        // ...
+      });
+
+
+  }
+
+  const handleGhSingIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(ghProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        setUser(user)
+        
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage ,errorCode , email , credential)
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSingIn}>  Sing In use in Google</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={handleFbSingIn}>Facebook Sing in</button>
+      <br />
+      <br /><br />
+      <br />
+      <button onClick={handleGhSingIn}>GitHab Sing in</button>
+
+      <p>User Name: {user.displayName}</p>
+      <img src={user.photoURL} alt=""/>
     </div>
   );
 }
---
+++ src/fierbase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5Fa9bmI_J6V4ym1j00lWq2cyv-OXPx2E",
+    authDomain: "auth-reca.firebaseapp.com",
+    projectId: "auth-reca",
+    storageBucket: "auth-reca.appspot.com",
+    messagingSenderId: "957127122087",
+    appId: "1:957127122087:web:f8cc329928787d28978f82",
+    measurementId: "G-E7C3RE10CQ"
+  };
+
+  export default firebaseConfig;
